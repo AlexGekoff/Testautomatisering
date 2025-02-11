63739e2663f662c082ea4ce8df8fae77e27f9c36
Aleksandra <aleksandra.gekoff@gmail.com>
2025-02-11 10:59:18 +0100

    Jag har lagt till felhantering och ändrat if-satsen till en switch-sats

--- src/Exercise1/MorseApp.java
+++ src/Exercise1/MorseApp.java
@@ -18,28 +18,29 @@ public class MorseApp {
             try {
                 int choice = Integer.parseInt(input);
 
-                if (choice == 1) {
-                    System.out.println("Ange text:");
-                    String text = scanner.nextLine();
-                    System.out.println("Morsekod: " + converter.toMorse(text));
-                } else if (choice == 2) {
-                    System.out.println("Ange morsekod (separera varje bokstav med mellanslag, ex: .... . .---):");
-                    String morse = scanner.nextLine();
-                    System.out.println("Text: " + converter.fromMorse(morse));
-                } else if (choice == 3) {
-                    System.out.println("Programmet avslutas.");
-                    break; //
-                } else {
-                    System.out.println("Ogiltigt val.");
+                switch (choice) {
+                    case 1:
+                        System.out.println("Ange text:");
+                        String text = scanner.nextLine();
+                        System.out.println("Morsekod: " + converter.toMorse(text));
+                        break;
+                    case 2:
+                        System.out.println("Ange morsekod (separera varje bokstav med mellanslag, ex: .... . .---):");
+                        String morse = scanner.nextLine();
+                        System.out.println("Text: " + converter.fromMorse(morse));
+                        break;
+                    case 3:
+                        System.out.println("Programmet avslutas.");
+                        scanner.close();
+                        return; // Gör en "return" för att avsluta programmet
+                    default:
+                        System.out.println("Ogiltigt val.");
                 }
             } catch (NumberFormatException e) {
-                System.out.println(("Fel: Du måste ange en siffra (1,2 eller 3)."));
+                System.out.println("Fel: Du måste ange en siffra (1, 2 eller 3).");
             } catch (IllegalArgumentException e) {
                 System.out.println("Fel: " + e.getMessage()); // Visa felmeddelande från undantag
             }
-
         }
-        scanner.close();
     }
 }
-
